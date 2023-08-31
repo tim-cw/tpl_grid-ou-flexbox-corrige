656a32ebacdf88901881d14657faf20e07e68a10
Matthieu Parent <matthieuparent@outlook.com>
2023-08-30 22:03:16 -0400

    updated files name

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,1 +1,14 @@
+/** Componsante Main de TimTools */
+export default class Main {
+  constructor(element) {
+    this.init();
+  }
 
+  /**
+   * Méthode d'initialisation
+   */
+  init() {
+    console.log('allo le curieux, on va jouer ici dans quelques semaines');
+  }
+}
+new Main();
